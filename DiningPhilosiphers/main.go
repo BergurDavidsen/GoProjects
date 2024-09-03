596574fd2fca982d4a830d04775ac826f0000a21
André <arbi@itu.dk>
2024-09-03 13:59:02 +0200

    made a struct for philosopher
    Co-Authored-By: BergurDavidsen <98157384+BergurDavidsen@users.noreply.github.com>
    Co-Authored-By: broh <3535317+broh@users.noreply.github.com>
    Co-Authored-By: Jason Zvaniga <11085495+koad@users.noreply.github.com>

--- DiningPhilosiphers/main.go
+++ DiningPhilosiphers/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-
 	//minMaxThinking := []int{2, 3, 4, 5, 6, 7}
 	forkChannel := make(chan [5]int)
 
@@ -19,16 +18,57 @@ func forks(id int, forksChannel chan [5]int) {
 	//<-forksChannel[id]
 }
 
+type Philosopher struct {
+	id                  int
+	leftFork, rightFork int
+}
+
 // Corrected function to accept a channel of integers
 func philosopher(id int, forksChannel chan [5]int) {
+	p := Philosopher{
+		id:        id,
+		leftFork:  -1,
+		rightFork: -1,
+	}
+
 	var rightFork int
 	var leftFork int
 
-	if bool() {
-		// hungry
+	if ranBool() {
+		p.rightFork = requestFork(id)
+		p.leftFork = requestFork(id % 4) // size of array 5-1
+
+		if rightFork != -1 || leftFork != -1 {
+			isEating(p.id)
+		} else {
+			isThinking(p.id)
+		}
 	} else {
-		// thinking
+		isThinking(id)
 	}
+
+}
+
+func requestFork(id int, forkChannel chan []int) int {
+	// return -1 if it doesn't work
+
+	return 0
+}
+
+func isThinking(id int) {
+	fmt.Printf("Philosopher ", id, " is thinking thoughts...")
+}
+
+func isEating(id int) {
+	fmt.Printf("Philosipher %v is eating pasta", id)
+}
+
+func timeLimit() int {
+	return rand.Intn(5) + 1
+}
+
+func ranBool() bool {
+	return rand.Intn(1) == 0
 }
 
 // Corrected initPhilosopher function
@@ -46,20 +86,4 @@ func initForks(forkChannel chan [5]int) {
 	go forks(2, forkChannel)
 	go forks(3, forkChannel)
 	go forks(4, forkChannel)
-}
-
-func isThinking(id int) {
-	fmt.Printf("Philosopher ", id, " is thinking thoughts...")
-}
-
-func isEating(id int) {
-	fmt.Printf("Philosipher %v is eating pasta", id)
-}
-
-func timeLimit() int {
-	return rand.Intn(5) + 1
-}
-
-func ranBool() bool {
-	return rand.Intn(1) == 0
 }
