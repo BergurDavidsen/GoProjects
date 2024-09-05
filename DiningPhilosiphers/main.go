027845eec830c8ff86cd8a168227d3738f8633ae
André <arbi@itu.dk>
2024-09-05 22:07:25 +0200

    The act of eating is serialized

--- DiningPhilosiphers/main.go
+++ DiningPhilosiphers/main.go
@@ -8,12 +8,14 @@ import (
 )
 
 var (
-	rounds = 1000
+	rounds      = 10
+	printResult = false
 )
 
 func main() {
 	var wg sync.WaitGroup
 	var mu sync.Mutex
+
 	fork0 := make(chan bool, 1)
 	fork1 := make(chan bool, 1)
 	fork2 := make(chan bool, 1)
@@ -32,17 +34,17 @@ func main() {
 	var p4 = Philosopher{id: 3, forkChan1: fork3, forkChan2: fork4}
 	var p5 = Philosopher{id: 4, forkChan1: fork4, forkChan2: fork0}
 
-	for i := 0; i < rounds; i++ {
-		wg.Add(5)
-		go p1.run(&wg, &mu)
-		go p2.run(&wg, &mu)
-		go p3.run(&wg, &mu)
-		go p4.run(&wg, &mu)
-		go p5.run(&wg, &mu)
-	}
+	wg.Add(5)
+	go p1.run(&wg, &mu)
+	go p2.run(&wg, &mu)
+	go p3.run(&wg, &mu)
+	go p4.run(&wg, &mu)
+	go p5.run(&wg, &mu)
 
 	wg.Wait()
-	fmt.Println(p1.eatCount, p2.eatCount, p3.eatCount, p4.eatCount, p5.eatCount)
+
+	fmt.Println("eating count")
+	fmt.Println("p1:", p1.eatCount, "\n", "p2:", p2.eatCount, "\n", "p3:", p3.eatCount, "\n", "p4:", p4.eatCount, "\n", "p5", p5.eatCount)
 
 }
 
@@ -51,17 +53,24 @@ type Philosopher struct {
 	fork1, fork2         bool
 	forkChan1, forkChan2 chan bool
 	eatCount             int
+	thinkCount           int
 }
 
 func (p *Philosopher) run(wg *sync.WaitGroup, mu *sync.Mutex) {
-
-	if rand.Intn(100)%2 == 0 {
-		eat(p, mu)
-		fmt.Println()
-	} else {
-		fmt.Println(p.id, "is thinking")
-		fmt.Println()
-		time.Sleep(5)
+	for i := 0; i < rounds; i++ {
+		if rand.Intn(100)%2 == 0 {
+			wg.Add(1)
+			go eat(p, mu)
+			wg.Done()
+		} else {
+			if printResult {
+				wg.Add(1)
+				go fmt.Println(p.id, "is thinking")
+				wg.Done()
+				fmt.Println()
+			}
+			time.Sleep(5000)
+		}
 	}
 
 	defer wg.Done()
@@ -71,13 +80,16 @@ func eat(p *Philosopher, mu *sync.Mutex) {
 	mu.Lock()
 	<-p.forkChan1
 	<-p.forkChan2
+	mu.Unlock()
 
 	time.Sleep(5)
-	fmt.Println(p.id, "is eating")
+	if printResult {
+		fmt.Println(p.id, "is eating")
+		fmt.Println()
+	}
 	p.eatCount++
 
 	p.forkChan1 <- true
 	p.forkChan2 <- true
 
-	mu.Unlock()
 }
