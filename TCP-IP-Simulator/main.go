a019e50f35dccf757a8971a8c09cfdcfc1a68810
André <arbi@itu.dk>
2024-09-17 11:32:43 +0200

    Update main.go
    Co-Authored-By: Konradmeno <57835836+Konradmeno@users.noreply.github.com>
    Co-Authored-By: Bror Hansen <149524351+SneekySnek123@users.noreply.github.com>
    Co-Authored-By: broh <3535317+broh@users.noreply.github.com>

--- TCP-IP-Simulator/main.go
+++ TCP-IP-Simulator/main.go
@@ -2,6 +2,77 @@ package main
 
 import "fmt"
 
+
+
 func main() {
-	fmt.Println("Hello, World!")
+	client_sender := make(chan string, 1)
+	client_receiver := make(chan string, 1)
+	client_verification := make(chan string, 1)
+
+	server_sender := make(chan string, 1)
+	server_receiver := make(chan string, 1)
+	server_verification := make(chan string, 1)
+
+	God := make(chan string, 1)
+
+	go client(client_sender, client_receiver, client_verification)
+	go server(server_sender, server_receiver, server_verification)
+	go forwarder(client_sender, client_receiver, client_verification, server_verification)
+	go God(logger)
+
+	hashmap := make(map[int]string)
+	
+
+}
+
+func client(sequence_number, sender, receiver, verification chan string) {
+	seq := 1 //starts with 1
+
+	//sends syn msg to sender channel
+	msg := getMessage(seq, "SYN")
+	fmt.Print("Client sending msg: ", msg)
+	sender <- msg
+
+	//waits to receive SYN-ACK from client
+	synaAck := <-receiver
+	fmt.Println("Client received: ", synAck)
+
+	msg = getMessage(seq+1, "ACK")
+	fmt.
+}
+
+func server(sequence_number, sender, receiver, verification chan string) {
+
+}
+
+
+func forwarder(client_sender, client_receiver, client_verification,
+	server_sender, server_receiver, server_verification chan string) {
+
+	for{
+		select{
+		case msg := <-client_sender:
+			fmt.Println()
+			server_receiver <- msg
+		}
+	}
+}
+
+func God(logger chan string){
+	for log := range logger{
+		fmt.Println(log)
+	}
+}
+
+func verification(sender, receiver, verification chan string) {
+
+}
+
+func getMessage(SEQno int, Type string) string {
+
+	return "fmt."
+}
+
+func randomSequenceNo() int {
+
 }
