31680015b6ba42403ca90438e577aee50c52e528
Bror <broh@itu.dk>
2024-09-30 11:06:26 +0200

    small changes

--- DiningPhilosiphers/main.go
+++ DiningPhilosiphers/main.go
@@ -15,19 +15,19 @@ func main() {
 	// Initialize fork channels with values
 	for i := 0; i < len(forksChannel); i++ {
 		forksChannel[i] = make(chan int, 1) // Use buffered channels
-		philChannel[i] = make(chan int, 1)
+		philChannel[i] = make(chan int, 2)
 		forksChannel[i] <- i
 	}
 
 	for i := 0; i < 5; i++ { // Increase the number of philosophers to match the number of forks
 		wg.Add(1)
-		go philosopher(i, wg)
-		go forks(i, wg)
+		go philosopher(i)
+		go forks(i)
 	}
 	wg.Wait()
 }
 
-func philosopher(id int, wg *sync.WaitGroup) {
+func philosopher(id int) {
 	left := id
 	right := (id + 1) % 5
 	forksChannel[left] <- left
@@ -49,7 +49,7 @@ func philosopher(id int, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-func forks(id int, wg *sync.WaitGroup) {
+func forks(id int) {
 	<-forksChannel[id]
 
 }
