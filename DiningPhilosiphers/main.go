36cdffd9c60cd84305f600003ad1ee4b1b45e15c
Bergur <berd@itu.dk>
2024-09-30 10:34:45 +0200

    joe

--- DiningPhilosiphers/main.go
+++ DiningPhilosiphers/main.go
@@ -2,149 +2,36 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"sync"
-	"time"
 )
 
-/**
- * The program is OOP, which idk is ok to do here.
- * This approach uses an odd/even fork extractment
- * The print booleans are if you want them printed
- * in the console
- */
-var ( // global var
-	rounds        = 10
-	group         = 5
-	forks         = group
-	waitingSeed   = 20000000 // random ms from 0-n
-	printActions  = !true
-	printChannels = !false
-	printResults  = !true
-	threadCount   = 0
-	wg            = &sync.WaitGroup{}
-)
-
-type (
-	Fork struct {
-		id       int
-		reserved bool
-	}
-
-	Philosopher struct {
-		id                   int
-		forkChan1, forkChan2 chan Fork
-		eatCount             int
-		thinkCount           int
-	}
+var (
+	wg           sync.WaitGroup
+	forksChannel = make([]chan bool, 5)
 )
 
 func main() {
-	if group == 1 { // just to make sure a philosopher wants to eat alone
-		forks = 2
+	// Initialize fork channels with values
+	for i := 0; i < len(forksChannel); i++ {
+		forksChannel[i] = make(chan bool, 1) // Use buffered channels
+		forksChannel[i] <- true              // Fork is initially available
 	}
 
-	forkArr := make([]chan Fork, forks)
-	groupArr := make([]Philosopher, group)
-
-	for i := 0; i < forks; i++ {
+	for i := 0; i < 5; i++ { // Increase the number of philosophers to match the number of forks
 		wg.Add(1)
-		forkArr[i] = make(chan Fork, 1) // make a channel for the Fork
-		f := Fork{id: i}
-		forkArr[i] <- f // insert fork into channel
-		go f.run()      // Start a go routine
+		go philosopher(i, &wg)
+		go forks(i, &wg)
 	}
-
-	for i := 0; i < group; i++ { // birth the philosophers
-		wg.Add(1)
-
-		groupArr[i] = Philosopher{
-			id:        i,
-			forkChan1: forkArr[i],
-			forkChan2: forkArr[(i+1)%forks], // make sure the last philosopher shares fork with index 0
-		}
-
-		go groupArr[i].run() // start program with the respective threads
-
-	}
-
 	wg.Wait()
-
-	printStats(groupArr)
 }
 
-func (p *Philosopher) run() {
-	defer wg.Done()
-	threadCount++
+func philosopher(id int, wg *sync.WaitGroup) {
 
-	for i := 0; i < rounds; i++ {
-		if rand.Intn(100)%2 == 0 { // 50% they eat
-			if p.id%2 == 0 { // to prevent deadlock, odd/even approach
-				eat(p, p.forkChan1, p.forkChan2)
-			} else {
-				eat(p, p.forkChan2, p.forkChan1)
-			}
-		} else { // 50% they think
-			think(p)
-		}
-	}
-}
-
-func eat(p *Philosopher, forkChan1 chan Fork, forkChan2 chan Fork) {
-	threadCount++
-
-	f1 := <-forkChan1 // fc 2
-	f2 := <-forkChan2 // fc 1
-
-	if printChannels {
-		fmt.Printf("Philosopher: %v, has aquired forks: %v, %v\n", p.id, f1.id, f2.id)
-	}
-
-	p.eatCount++
-	time.Sleep(randomMillis(waitingSeed))
-
-	if printActions {
-		fmt.Println(p.id, "is eating\n")
-	}
-
-	forkChan1 <- f1 // f2
-	forkChan2 <- f2 // f1
-	if printChannels {
-		fmt.Printf("Philosopher: %v, has released forks: %v, %v\n", p.id, f1.id, f2.id)
-	}
-
-}
-
-func (f *Fork) run() {
-	threadCount++
+	fmt.Println("joe")
 	wg.Done()
-	wg.Wait()
 }
 
-func think(p *Philosopher) {
-	p.thinkCount++
-	if printActions {
-		fmt.Println(p.id, "is thinking\n")
-	}
-	time.Sleep(randomMillis(waitingSeed))
-}
+func forks(id int, wg *sync.WaitGroup) {
 
-func randomMillis(seed int) time.Duration {
-	return time.Duration(rand.Intn(seed))
-}
-
-func (p *Philosopher) toString() string {
-	return fmt.Sprintf("Philosopher %v ----\n ate: %v times\n though: %v times\n", p.id, p.eatCount, p.thinkCount)
-}
-
-func printStats(array []Philosopher) {
-	fmt.Println("threadcount:", threadCount)
-
-	if printResults {
-		fmt.Println("Stats\n")
-		for i := 0; i < len(array); i++ {
-			fmt.Println(array[i].toString())
-		}
-	}
-
+	fmt.Println("joe")
 }
