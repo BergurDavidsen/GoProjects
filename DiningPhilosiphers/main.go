44f9eb2dea646cec93f26077393debccffa4b81c
Bergur <berd@itu.dk>
2024-09-30 11:02:25 +0200

    Added basic functioanlity

--- DiningPhilosiphers/main.go
+++ DiningPhilosiphers/main.go
@@ -6,21 +6,23 @@ import (
 )
 
 var (
-	wg           sync.WaitGroup
+	wg           = &sync.WaitGroup{}
 	forksChannel = make([]chan int, 5)
+	philChannel  = make([]chan int, 5)
 )
 
 func main() {
 	// Initialize fork channels with values
 	for i := 0; i < len(forksChannel); i++ {
 		forksChannel[i] = make(chan int, 1) // Use buffered channels
-		forksChannel[i] <- i                // Fork is initially available
+		philChannel[i] = make(chan int, 1)
+		forksChannel[i] <- i
 	}
 
 	for i := 0; i < 5; i++ { // Increase the number of philosophers to match the number of forks
 		wg.Add(1)
-		go philosopher(i, &wg)
-		go forks(i, &wg)
+		go philosopher(i, wg)
+		go forks(i, wg)
 	}
 	wg.Wait()
 }
@@ -28,15 +30,26 @@ func main() {
 func philosopher(id int, wg *sync.WaitGroup) {
 	left := id
 	right := (id + 1) % 5
+	forksChannel[left] <- left
+	forksChannel[right] <- right
 
-	<-forksChannel[left]
-	<-forksChannel[right]
+	for i := 0; i < 5; i++ {
 
-	fmt.Println("Eating", id)
+		select {
+		case <-forksChannel[left]:
+			select {
+			case <-forksChannel[right]:
+				fmt.Printf("%d is eating", id)
+			default:
+				fmt.Printf("%d is thinking", id)
+			}
+		}
+	}
 
 	wg.Done()
 }
 
 func forks(id int, wg *sync.WaitGroup) {
+	<-forksChannel[id]
 
 }
