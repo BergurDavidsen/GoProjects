cb6c22bc31487d3f3c890bc44dc7bc83f1cb46f9
Bror Hansen <broh@itu.dk>
2024-10-24 17:56:44 +0200

    yeetus

--- Chitty-Chat/Server.go
+++ Chitty-Chat/Server.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"log"
+	"net"
+	"os"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	s := grpc.NewServer()
+	
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", port, err)
+	}
+	log.Println("Server listening on port", port)
 
-	pb.RegisterGreeterServer(s, &server{})
+	grpcserver = grpc.NewServer()
+	err = grpcserver.Serve(listener)
+	if err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
